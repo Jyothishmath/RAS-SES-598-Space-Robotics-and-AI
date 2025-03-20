c4f8a2b0e53033fbbb0034ee8c672264b8e8fcfa
Jyothishmath <115547456+Jyothishmath@users.noreply.github.com>
2025-03-19 23:29:01 -0700

    Update lqr_controller.py

--- assignments/cart_pole_optimal_control/cart_pole_optimal_control/lqr_controller.py
+++ assignments/cart_pole_optimal_control/cart_pole_optimal_control/lqr_controller.py
@@ -33,8 +33,8 @@ class CartPoleLQRController(Node):
         ])
         
         # LQR cost matrices
-        self.Q = np.diag([1.0, 1.0, 10.0, 10.0])  # State cost
-        self.R = np.array([[0.1]])  # Control cost
+        self.Q = np.diag([200, 80, 15.0, 150.0])  # State cost
+        self.R = np.array([[2.5]])  # Control cost
         
         # Compute LQR gain matrix
         self.K = self.compute_lqr_gain()
